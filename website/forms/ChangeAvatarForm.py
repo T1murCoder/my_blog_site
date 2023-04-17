60f87a7b64f27580527d8d5f95e3b663027a5ac2
T1murCoder <timur.zarutskiy@yandex.ru>
2023-04-17 20:12:14 +0300

    Сделал проверку расширения файла

--- website/forms/ChangeAvatarForm.py
+++ website/forms/ChangeAvatarForm.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, EmailField, TextAreaField, FileField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
 
 
 class ChangeAvatarForm(FlaskForm):
-    avatar = FileField("Прикрепите изображение", validators=[DataRequired()])
+    avatar = FileField("Прикрепите изображение", validators=[DataRequired(), FileAllowed(['jpg', 'png'], 'Неправильное расширение файла!')])
     submit = SubmitField('Изменить')
